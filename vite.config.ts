286eff25b55c4727956768eec877fccf7129ee2f
Mohamed Almetwaly <almetwaly088@gmail.com>
2026-06-08 19:47:31 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HR/', 
+  base: '/HR/', // أضف هذا السطر بالضبط
 })
